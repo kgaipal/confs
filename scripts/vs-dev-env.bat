22b906cc030feaa5c2074332748039b9cb2fab90
Kshitij Gaipal <kgaipal@gmail.com>
2016-07-14 16:21:05 -0700

    [script] vs-dev-env.bat is turning ECHO OFF in the beginning rather after comments

--- scripts/vs-dev-env.bat
+++ scripts/vs-dev-env.bat
@@ -1,11 +1,12 @@
-rem 
+ECHO OFF
+
+rem
 rem Kshitij Gaipal
 rem kgaipal@gmail.com
-rem 
+rem
 rem Sets the custom prompt for cmd.exe and then exec's Visual Studio dev environment for
 rem current shell.
 
-ECHO OFF
 CHDIR "%HOME%/code/"
-set PROMPT=[$P]$_$$ 
+set PROMPT=[$P]$_$$
 %comspec% /k "C:/"Program Files (x86)"/"Microsoft Visual Studio 14.0"/Common7/Tools/VsDevCmd.bat"
