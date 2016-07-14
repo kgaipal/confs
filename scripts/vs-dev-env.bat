b835c290b33871ca0c19a3d11b35bbcfab953fdc
Kshitij Gaipal <kgaipal@gmail.com>
2016-07-14 11:03:56 -0700

    [script] Added a config file which sets a custom PROMPT and exec's visual studio dev environment before continuing

---
+++ scripts/vs-dev-env.bat
@@ -0,0 +1,11 @@
+rem 
+rem Kshitij Gaipal
+rem kgaipal@gmail.com
+rem 
+rem Sets the custom prompt for cmd.exe and then exec's Visual Studio dev environment for
+rem current shell.
+
+ECHO OFF
+CHDIR "%HOME%/code/"
+set PROMPT=[$P]$_$G 
+%comspec% /k "C:/"Program Files (x86)"/"Microsoft Visual Studio 14.0"/Common7/Tools/VsDevCmd.bat"
