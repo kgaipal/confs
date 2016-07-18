453cbeb2e8bb848d7addbe50b275954fdd626d59
Kshitij Gaipal <kgaipal@gmail.com>
2016-07-18 10:51:47 -0700

    [scripts] Moved the cmd.exe alias and prompt configuration into a separate env.cmd file and edited registry to include it every time

---
+++ scripts/env.cmd
@@ -0,0 +1,23 @@
+::
+:: Kshitij Gaipal
+:: kgaipal@gmail.com
+::
+:: Sets the custom prompt, path, etc for cmd.exe and also defines some aliases like bash
+:: http://stackoverflow.com/questions/20530996/aliases-in-windows-command-prompt#20531778
+
+:: Should be included this way so that each instance of cmd.exe loads
+:: [HKEY_CURRENT_USER\Software\Microsoft\Command Processor]
+:: "AutoRun"="%USERPROFILE%\\code\\confs\\scripts\\env.cmd"
+
+
+@ECHO OFF
+
+:: Add "~/.local/bin" and some usefull unix tools like grep, find, etc available
+set PATH="%USERPROFILE%\.local\bin\";"%USERPROFILE%\code\git-sdk-64\usr\bin";%PATH%
+
+:: Set prompt
+set PROMPT=[$P]$_$$
+
+:: Commands
+DOSKEY b=CHDIR ../
+DOSKEY ll=DIR /N
--- scripts/vs-dev-env.bat
+++ scripts/vs-dev-env.bat
@@ -1,12 +1,8 @@
-@ECHO OFF
-
-rem
-rem Kshitij Gaipal
-rem kgaipal@gmail.com
-rem
-rem Sets the custom prompt for cmd.exe and then exec's Visual Studio dev environment for
-rem current shell.
+::
+:: Kshitij Gaipal
+:: kgaipal@gmail.com
+::
+:: Exec's Visual Studio dev environment for current shell.
 
 CHDIR "%HOME%/code/"
-set PROMPT=[$P]$_$$
 %comspec% /k "C:/"Program Files (x86)"/"Microsoft Visual Studio 14.0"/Common7/Tools/VsDevCmd.bat"
