ed78919f771455c443206629a93ae9f1c5b4a53f
Kshitij Gaipal <kgaipal@gmail.com>
2016-07-20 11:20:19 -0700

    Explicitly calling 'env.cmd' because calling vs-dev-env.bat doesnt work in emacs -shell'

--- scripts/vs-dev-env.bat
+++ scripts/vs-dev-env.bat
@@ -4,5 +4,6 @@
 ::
 :: Exec's Visual Studio dev environment for current shell.
 
+CALL env.cmd
 CHDIR "%HOME%/code/"
 %comspec% /k "C:/"Program Files (x86)"/"Microsoft Visual Studio 14.0"/Common7/Tools/VsDevCmd.bat"
