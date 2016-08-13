01fc9dc3436f1f8b6602539ce326104e449f2ab5
Kshitij Gaipal <kgaipal@gmail.com>
2016-08-13 15:27:54 -0700

    [windows dev conf] Writing to bashrc from windows bat script itself

--- scripts/windows-setup-dev-env.bat
+++ scripts/windows-setup-dev-env.bat
@@ -1,8 +1,9 @@
 :: Sets up various shortcuts and environment variables
 :: Run as administrator
 
+@ECHO OFF
 SET HOME=%USERPROFILE%
-SET START_MENU=%HOME%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs
+SET START_MENU=%HOME%/Start Menu/Programs
 
 MKDIR "%HOME%/code"
 MKDIR "%HOME%/.local/bin"
@@ -12,12 +13,12 @@ MKDIR "%HOME%/.local/bin"
 %COMSPEC% /C "git clone https://github.com/kgaipal/confs.git "%HOME%/code/confs""
 
 :: Note: to make soft links, use MKLINK instead of bash's ln otherwise
-:: git-bash will copy target file to the link location instead [bug #???]
+:: git-bash will copy target file to the link location instead [git bug #???]
 MKLINK "%HOME%/.local/bin/grepk" "%HOME%/code/confs/scripts/grepk"
 MKLINK "%HOME%/.local/bin/findk" "%HOME%/code/confs/scripts/findk"
 MKLINK "%HOME%/.local/bin/util.inc" "%HOME%/code/confs/scripts/util.inc"
+MKLINK "%HOME%/.local/bin/vs-cmd" "%HOME%/code/confs/scripts/vs-dev-env.bat"
 MKLINK "%HOME%/.gitconfig" "%HOME%/code/confs/misc/gitconfig"
-MKLINK "%HOME%/.bashrc" "%HOME%/code/confs/misc/gitconfig"
 
 :: remove unwanted programs which came with vanilla installation
 %COMSPEC% /C "powershell -ExecutionPolicy ByPass -File windows10-remove-default-pacakges.ps1"
@@ -29,9 +30,19 @@ MKLINK "%HOME%/.bashrc" "%HOME%/code/confs/misc/gitconfig"
 
 :: create shortcut for other scripts for start menu only
 :: disabling thi now this since wee want to create a shortcut not symlinks!
-::MKLINK "%START_MENU%/vs-cmd" "%HOME%/code/confs/scripts/vs-dev-env.bat"
+rem MKLINK "%START_MENU%/vs-cmd" "%HOME%/code/confs/scripts/vs-dev-env.bat"
 
-:: prefer to call a git-bash and setup all commands in that instead
-
+:: bashrc specific
+(
+ECHO # extensions to bashrc
+ECHO BASHRC_EXTS=$HOME/code/confs/misc/
+ECHO if [ -f $BASHRC_EXTS/bashrc_append ]; then
+ECHO    . $BASHRC_EXTS/bashrc_append
+ECHO fi
+ECHO # alias definitions
+ECHO if [ -f $BASHRC_EXTS/bash_aliases ]; then
+ECHO   . $BASHRC_EXTS/bash_aliases
+ECHO fi
+) > "%HOME%/.bashrc"
 
 ECHO Done setting up
