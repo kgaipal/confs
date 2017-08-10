ba5c6df9c7d8bff5ea982126da2d5bf4b0c7c0a1
Kshitij Gaipal <kgaipal@gmail.com>
2017-08-10 13:19:26 -0700

    made this callback from emacs shell

--- scripts/vs-dev-env.bat
+++ scripts/vs-dev-env.bat
@@ -3,5 +3,4 @@
 ::
 :: Exec's Visual Studio dev environment for current shell.
 
-CHDIR "%USERPROFILE%/code/"
-CALL "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/Tools/VsDevCmd.bat"
+%comspec% /k ""C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/Tools/VsDevCmd.bat""
