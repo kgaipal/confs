4e4ac99278015e706e535287907c139dc61fc627
Kshitij Gaipal <ksgaipal@gmail.com>
2018-01-10 10:42:05 -0800

    moved some variable

--- scripts/pull-cosmos-data-for-a-day.sh
+++ scripts/pull-cosmos-data-for-a-day.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+# this script overwrite the following xml tag in config file
 # <add key="fileNumber" value="-1"/>
 
-CONFIG_FILE="c://msys64//home//ksgaipal//Documents//Cosmos//PullDataFromCosmos//iscopeTool.exe.config"
-PROGRAM_EXE="C://Users//ksgaipal//Documents//Cosmos//PullDataFromCosmos//iscopeTool.exe"
-# CMD_EXE="START cmd.exe /C "
-CMD_EXE="C://msys64//mingw64.exe"
+SCOPE_APP="C://Users//ksgaipal//Documents//Cosmos//PullDataFromCosmos//iscopeTool.exe"
+SCOPE_APP_CONFIG_FILE=${SCOPE_APP}".config"
+TERMINAL_APP="C://msys64//mingw64.exe"
 
 function create_xml_tag
 {
     echo "<add key=\"fileNumber\" value=\"$1\"\/>"
+}
+
+function print_variables
+{
+    echo "following config for this script"
+    echo $SCOPE_APP
+    echo $SCOPE_APP_CONFIG_FILE
+    echo $TERMINAL_APP
 }
 
 function launch_cmd_in_sequence
@@ -19,21 +27,21 @@ function launch_cmd_in_sequence
     do
         # prepare search format
         local num2=`printf "%02d" $num`
-        echo "$prev_num -> $num2"
         local search_text=`create_xml_tag $prev_num`
         local replace_text=`create_xml_tag $num2`
 
         # replace now
-        sed -i "s/$search_text/$replace_text/g" $CONFIG_FILE
+        sed -i "s/$search_text/$replace_text/g" $SCOPE_APP_CONFIG_FILE
 
-        # launch program
-        eval $CMD_EXE $PROGRAM_EXE
-        sleep 30s
+        # launch program for each hour every 1 min
+        echo "working on hour: $num2"
+        eval $TERMINAL_APP $SCOPE_APP
+        sleep 1m
 
         # update counter
         prev_num=$num2
-
     done
 }
 
+print_variables
 launch_cmd_in_sequence
