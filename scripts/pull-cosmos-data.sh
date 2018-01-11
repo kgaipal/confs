6952ebd7d9a1ead00c84dd4753d27d01bb79dc6e
Kshitij Gaipal <ksgaipal@gmail.com>
2018-01-11 13:06:41 -0800

    added range function (not complete)

--- scripts/pull-cosmos-data.sh
+++ scripts/pull-cosmos-data.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-# this script overwrite the following xml tag in config file
+# this script overwrite the following xml tag in config file and keeps launching processes
+# need this start condition
 # <add key="fileNumber" value="-1"/>
-BASE_DIR="C://Users//ksgaipal//Documents"
-SCOPE_APP="${BASE_DIR}/Cosmos//PullDataFromCosmos//iscopeTool.exe"
+
+BASE_APP_DIR="C://Users//ksgaipal//Documents//Cosmos"
+COSMOS_DATA_DIR_PREFIX="c:/msys64/home/ksgaipal/Downloads/CosmosData_201712"
+
+# pull cosomos data into xml files specific
+SCOPE_APP="${BASE_APP_DIR}//PullDataFromCosmos//iscopeTool.exe"
 SCOPE_APP_CONFIG_FILE=${SCOPE_APP}".config"
+
+# push xml files to SQL specific
+SCOPE_SQL_APP="${BASE_APP_DIR}//XportCosmosXMLToDB//ScopeXmlUpdate.exe"
+SCOPE_SQL_APP_CONFIG_FILE=${SCOPE_SQL_APP}".config"
+
+# terminal to launch (windows, bash, etc)
 TERMINAL_APP="C://msys64//mingw64.exe"
+
+# all 24 hours
 MAX_HOUR=23
+
+function create_day_format
+{
+    echo `printf "%02d" $1`
+}
+
+function create_hour_format
+{
+    echo `printf "%02d" $1`
+}
 
 function create_scope_file_number_xml_tag
 {
@@ -20,68 +43,78 @@ function create_scope_file_date_xml_tag
 
 function create_scope_file_save_location_xml_tag
 {
-    echo "<add key =\"storecosmosfilelocation\" value =\"$1\"\/>"
+    echo "<add key =\"storecosmosfilelocation\" value =\"${COSMOS_DATA_DIR_PREFIX}$1\"\/>"
 }
 
 function create_scope_file_read_location_xml_tag
 {
-    echo "<add key=\"sourcedir\" value=\"$1\"\/>"
+    echo "<add key=\"sourcedir\" value=\"${COSMOS_DATA_DIR_PREFIX}$1\"\/>"
 }
 
 function print_variables
 {
     echo "Script variables:"
-    echo -e "* SCOPE_APP: " $SCOPE_APP
+    echo -e "* SCOPE_APP:        " $SCOPE_APP
     echo -e "* SCOPE_APP_CONFIG: " $SCOPE_APP_CONFIG_FILE
-    echo -e "* TERMINAL: " $TERMINAL_APP
+    echo -e "* TERMINAL:         " $TERMINAL_APP
+    echo -e "* COSMOS_DATA_DIR:  " $COSMOS_DATA_DIR_PREFIX
     echo
 }
 
 function pull_for_a_day
 {
-    local prev_num=-1
+    local prev_hour=-1
+
     for num in `seq 0 1 $MAX_HOUR`
     do
         # prepare search format
-        local num2=`printf "%02d" $num`
-        local search_text=`create_scope_file_number_xml_tag $prev_num`
-        local replace_text=`create_scope_file_number_xml_tag $num2`
+        local hour=`create_hour_format $num`
+        local search_text=`create_scope_file_number_xml_tag $prev_hour`
+        local replace_text=`create_scope_file_number_xml_tag $hour`
 
         # replace now
         sed -i "s/$search_text/$replace_text/g" $SCOPE_APP_CONFIG_FILE
 
         # launch program for each hour every 1 min
-        echo "starting hour: $num2"
+        echo -n "starting for $hour hour..."
         eval $TERMINAL_APP $SCOPE_APP
+        echo " done"
         sleep 5m
 
         # update counter
-        prev_num=$num2
+        prev_hour=$hour
     done
 
     echo "done [day]."
 }
 
-function pull_for_days_range
+function pull_for_days_in_range
 {
     local prev_day=-1
     local start_day=$1
     local end_day=$2
-    for num in `seq $START_DAY 1 $END_DAY`
-    do
-        # prepare search format
-        local day=`printf "%02d" $num`
-        local search_text=`create_scope_file_number_xml_tag $prev_day`
-        local replace_text=`create_scope_file_number_xml_tag $day`
 
-        # replace now
+    for num in `seq $start_day 1 $end_day`
+    do
+        local day=`create_day_format $num`
+        local search_text=""
+        local replace_text=""
+
+        # replace date
+        search_text=`create_scope_file_date_xml_tag $prev_day`
+        replace_text=`create_scope_file_date_xml_tag $day`
+        sed -i "s/$search_text/$replace_text/g" $SCOPE_APP_CONFIG_FILE
+
+        # replace downloaded file location
+        search_text=`create_scope_file_save_location_xml_tag $prev_day`
+        replace_text=`create_scope_file_save_location_xml_tag $day`
         sed -i "s/$search_text/$replace_text/g" $SCOPE_APP_CONFIG_FILE
 
         # launch program for each hour every 1 min
-        echo
+        # echo
         echo "starting day: $day"
         # pull_for_a_day
-        echo
+        # echo
 
         # update counter
         prev_day=$day
@@ -90,9 +123,16 @@ function pull_for_days_range
     echo "done [days-range]."
 }
 
+function push_to_sql
+{
+    echo "**TODO**"
+}
+
 print_variables
 
-# pull data from days inclusive in range [start, end]
-pull_for_days_range 28 29
+# # pull data from days inclusive in range [start, end]
+# pull_for_days_in_range 28 31
+pull_for_a_day
 
+# push to sql
 push_to_sql
