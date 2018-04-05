8d55a34e7fd043a089bbf2a57e037b8214a415be
Kshitij Gaipal <ksgaipal@gmail.com>
2018-04-05 11:11:33 -0700

    upgraded tool chain script of vs2017

--- scripts/vs-make.bat
+++ scripts/vs-make.bat
@@ -3,5 +3,9 @@
 ::
 :: Exec's msbuild from current shell.
 
-@CALL "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/Tools/VsDevCmd.bat"
+:: VS 2015
+rem @CALL "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/Tools/VsDevCmd.bat"
+
+:: VS 2017
+@CALL "C:/Program Files (x86)/./Microsoft Visual Studio/2017/Enterprise/Common7/Tools/VsDevCmd.bat"
 CALL "msbuild" "/v:minimal" %*
