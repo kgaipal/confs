d4326bf6fd6b0e72e6d299ccadcce182b6f69004
Kshitij Gaipal <ksgaipal@gmail.com>
2018-04-12 11:34:49 -0700

    sf cluster script chang and gitconfig change for aliases

--- scripts/reset-service-fabric-local-cluster.ps1
+++ scripts/reset-service-fabric-local-cluster.ps1
@@ -1,2 +1,3 @@
 # execute in admin in powershell
 & "$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\ClusterSetup\DevClusterSetup.ps1" -PathToClusterDataRoot "D:\SfDevCluster\Data"  -PathToClusterLogRoot "D:\SfDevCluster\Log" -CreateOneNodeCluster
+& Remove-Item -Path "C:\SfDevCluster" -Recurse
