6ced0edf08643e591fb82ca94623c54df2aed92f
Kshitij Gaipal <ksgaipal@gmail.com>
2018-06-10 07:36:04 -0700

    checing and removing C when needed

--- scripts/reset-service-fabric-local-cluster.ps1
+++ scripts/reset-service-fabric-local-cluster.ps1
@@ -1,3 +1,11 @@
+
 # execute in admin in powershell
 & "$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\ClusterSetup\DevClusterSetup.ps1" -PathToClusterDataRoot "D:\SfDevCluster\Data"  -PathToClusterLogRoot "D:\SfDevCluster\Log" -CreateOneNodeCluster
-& Remove-Item -Path "C:\SfDevCluster" -Recurse
+
+# remove C: after previous setup
+$SFCLUSTER_C = "C:\SfDevCluster"
+if (Test-Path -Path $SFCLUSTER_C ) {
+   & Remove-Item -Path $SFCLUSTER_C -Recurse
+}
+
+& Write-Host "Done"
