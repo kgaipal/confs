99888452036abc8be08cd3320f3f2a52da3d8925
Kshitij Gaipal <ksgaipal@gmail.com>
2018-09-17 14:59:48 -0700

    whtiespace

--- scripts/reset-service-fabric-local-cluster.ps1
+++ scripts/reset-service-fabric-local-cluster.ps1
@@ -1,6 +1,7 @@
+# run as admin
 
 # execute in admin in powershell
-& "$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\ClusterSetup\DevClusterSetup.ps1" -PathToClusterDataRoot "D:\SfDevCluster\Data"  -PathToClusterLogRoot "D:\SfDevCluster\Log" -CreateOneNodeCluster
+& "$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\ClusterSetup\DevClusterSetup.ps1" -PathToClusterDataRoot "D:\SfDevCluster\Data" -PathToClusterLogRoot "D:\SfDevCluster\Log" -CreateOneNodeCluster
 
 # remove C: after previous setup
 $SFCLUSTER_C = "C:\SfDevCluster"
